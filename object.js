327edd7195d75702885d04419260ca8ec88a932a
selimreza777 <selimrezacse@gmail.com>
2025-08-19 16:02:14 +0600

    js bonous repo create

---
+++ object.js
@@ -0,0 +1,13 @@
+const student = {
+  name: 'shakib khan',
+  id: 123,
+  address: 'movie cinema',
+  isSingle: true,
+  friends: ['apu', 'buli', 'sabila-nur', 'nabila', 'srabonti'],
+  car: {
+    brand: 'tesla',
+    price: 5000000,
+    made: 2025
+  }
+}
+console.log(student.car.price);
