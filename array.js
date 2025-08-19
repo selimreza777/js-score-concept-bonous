923e755695211bfecadc183b59747d124c14a8d9
selimreza777 <selimrezacse@gmail.com>
2025-08-19 16:51:43 +0600

    array vitor objcet

---
+++ array.js
@@ -0,0 +1,20 @@
+// products name ekta array declear korchi 
+// const products = [
+//   { name: 'bottol', price: 300, brand: 'rfl' },
+//   { name: 'phone', price: 5000, brand: 'vivo' },
+//   { name: 'laptop', price: 6700, brand: 'hp' },
+//   { name: 'bike', price: 150000, brand: 'royal' }
+// ]
+// // products[0];
+// // products[1];
+// console.log(products[3])
+
+// product name ekta object declear korchi 
+// const products = [15, 56, 87];
+const products = {
+  '0': 15,
+  '1': 56,
+  '2': 87
+
+}
+console.log(products[2])
--- object.js
+++ object.js
@@ -1,13 +1,23 @@
-const student = {
+const nayok = {
   name: 'shakib khan',
   id: 123,
   address: 'movie cinema',
   isSingle: true,
   friends: ['apu', 'buli', 'sabila-nur', 'nabila', 'srabonti'],
+  movies: [{ name: 'no-1', year: 2015 }, { name: 'tufan', year: 2024 }, { name: 'borbad', year: 2025 }, { name: 'priyo-toma', year: 2023 }],
+  act: function () {
+    console.log('You watch movie every Eid festivle');
+  },
   car: {
     brand: 'tesla',
     price: 5000000,
-    made: 2025
+    made: 2025,
+    manufaturer: {
+      name: 'tesla101',
+      ceo: 'elon-mask',
+      country: 'usa'
+    }
   }
 }
-console.log(student.car.price);
+// console.log(student.movies);
+nayok.act();
